b881b124a62a361e0cb19ad392d032836f2c6f53
qsparks <q.m.sparks@gmail.com>
2020-10-07 19:03:04 -0500

    adds and roughs out profile html

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+    return render(request, 'profile/index.html')
