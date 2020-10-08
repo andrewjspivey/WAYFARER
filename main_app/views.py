e4a26164eab203e9656c90f1f678ef08bef031dc
Binay bk <khadkabinay@yahoo.com>
2020-10-07 20:14:30 -0500

    updates views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import City,Post
+from django.contrib.auth import login
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>WAYFARER</h1>')
+    return render( request ,'home.html')
+
+
+def about(request):
+    return HttpResponse( '<h1>About</h1>')
+
+
+def user_detail(request):
+    return HttpResponse( '<h1>user_detail</h1>')
+
+
+def cities_index(request):
+    return HttpResponse( '<h1>cities_index</h1>')
+
+
+
+def cities_detail(request):
+    return HttpResponse( '<h1>cities_detail</h1>')
+
+
+def cities_delete(request):
+    return HttpResponse( '<h1>cities_delete</h1>')
+
+    
+def cities_edit(request):
+    return HttpResponse( '<h1>cities_edit</h1>')
+
+    
+def signup(request):
+    return HttpResponse( '<h1>signup</h1>')
+
+
+
+
+
+
