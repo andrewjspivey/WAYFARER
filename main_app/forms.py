d4d39801a84ab76492890b8ec77333e686d7ea82
qsparks <q.m.sparks@gmail.com>
2020-10-08 16:09:33 -0500

    reconfigures signup form to get all required information and create user's profile object

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,20 +1,21 @@
 from django.forms import ModelForm
-from .models import City, Post, Profile
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import City, Post, Profile
 
 
 class Register_Form(UserCreationForm):
     first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    current_city = forms.ModelChoiceField(queryset=City.objects.all())
+
     class Meta:
         model = User
-        fields = ['first_name']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'current_city']
         
-
-    
 
 class City_Form(ModelForm):
     class Meta:
