2bc88489e27a97c34a51accb55669bf70ccde29d
Binay bk <khadkabinay@yahoo.com>
2020-10-08 22:00:47 -0500

    updates on cities/detail.html

--- main_app/views.py
+++ main_app/views.py
@@ -93,3 +93,4 @@ def signup(request):
 
 
 
+
--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +126,7 @@ STATIC_URL = '/static/'
 
 # profile_detail
 # LOGIN_REDIRECT_URL = '/profile/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/profile/'
 
 # home
 LOGOUT_REDIRECT_URL = '/'
