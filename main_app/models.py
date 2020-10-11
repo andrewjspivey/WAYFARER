0c43d6e572294dad84e7b6b4a53b86d42209cf91
Binay bk <khadkabinay@yahoo.com>
2020-10-11 02:18:07 -0500

    adds relative published date on post and remains small fixes

--- main_app/models.py
+++ main_app/models.py
@@ -30,6 +30,31 @@ class Post(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+    def get_date(self):
+        time = datetime.now()
+        print(time)
+        if self.post_date.minute == time.minute:
+            return str(self.post_date.second - time.second) + " seconds ago"
+        elif self.post_date.hour == time.hour:
+            return str(self.post_date.minute - time.minute) + " minutes ago"
+        elif self.post_date.day == time.day:
+            return str(self.post_date.hour - time.hour) + " hours ago"
+        else:
+            if self.post_date.month == time.month:
+                return str(self.post_date.day - time.day ) + " days ago"
+            else:
+                if self.post_date.year == time.year:
+                    return str( self.post_date.month - time.month ) + " months ago"
+        return self.post_date 
+
+
+
+
+
+
+
+
     def __str__(self):
         return f"{self.user}'s {self.city.name} Post : {self.title} submitted on {self.post_date} : {self.content}"
 
