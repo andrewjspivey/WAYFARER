c0cf62ba173c2582ffc72809a96e43fd1930090a
Binay bk <khadkabinay@yahoo.com>
2020-10-11 17:22:41 -0500

    migrates to update models.py

--- main_app/models.py
+++ main_app/models.py
@@ -26,11 +26,7 @@ class Profile(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=250)
-<<<<<<< HEAD
-    content = models.TextField(max_length=2000 ,blank=False)
-=======
     content = models.TextField(max_length=5000 ,blank=False)
->>>>>>> submaster
     post_date = models.DateTimeField(auto_now_add = True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
